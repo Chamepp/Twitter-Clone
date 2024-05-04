24e15a5a794d636ca84e2901edd3c502b37bf7c2
System Administrator <ashkanebtekari@gmail.com>
2024-05-04 13:45:31 +0330

    started the project

---
+++ Twitter-Clone/Twitter-Clone/Controller/MainTabController.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabController.swift
+//  Twitter-Clone
+//
+//  Created by Ashkan Ebtekari on 5/3/24.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+    // MARK: - Helpers
+    func configureViewControllers() {
+        
+    }
+}

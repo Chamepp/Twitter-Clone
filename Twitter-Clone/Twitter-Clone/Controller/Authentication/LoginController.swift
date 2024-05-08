936131ceca2ea22faf1fbd5f9e110e62581e915e
System Administrator <ashkanebtekari@gmail.com>
2024-05-08 14:06:58 +0330

    added text inputs

--- Twitter-Clone/Twitter-Clone/Controller/Authentication/LoginController.swift
+++ Twitter-Clone/Twitter-Clone/Controller/Authentication/LoginController.swift
@@ -17,6 +17,34 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "mail")
+        
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -35,5 +63,12 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
     }
 }
---
+++ Twitter-Clone/Twitter-Clone/Utils/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  Twitter-Clone
+//
+//  Created by Ashkan Ebtekari on 5/8/24.
+//
+
+import Foundation

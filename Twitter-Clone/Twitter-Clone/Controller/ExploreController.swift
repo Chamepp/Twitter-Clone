79b4516e9a022cc94a230b0970c201514f12ff27
System Administrator <ashkanebtekari@gmail.com>
2024-05-25 11:03:56 +0330

    completed user search table view

--- Twitter-Clone/Twitter-Clone/Controller/ExploreController.swift
+++ Twitter-Clone/Twitter-Clone/Controller/ExploreController.swift
@@ -8,20 +8,49 @@
 
 import UIKit
 
-class ExploreController: UIViewController {
+private let reuseIdentifier = "UserCell"
+
+class ExploreController: UITableViewController {
     // MARK: - Properties
+    private var users = [User]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     // MARK: - Lifecycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
     }
     
+    // MARK: - API
+    func fetchUsers() {
+        UserService.shared.fetchUsers { users in
+            self.users = users
+        }
+    }
+    
     // MARK: - Helpers
     func configureUI() {
         view.backgroundColor = .white
         navigationItem.title = "Explore"
+        
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+extension ExploreController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        cell.user = users[indexPath.row]
+        return cell
     }
 }
---
+++ Twitter-Clone/Twitter-Clone/View/UserCell.swift
@@ -0,0 +1,74 @@
+//
+//  UserCell.swift
+//  Twitter-Clone
+//
+//  Created by Ashkan Ebtekari on 5/25/24.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    // MARK: - Properties
+    var user: User? {
+        didSet {
+            configure()
+        }
+    }
+    
+    
+    private lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 40, height: 40)
+        iv.layer.cornerRadius = 40 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let stack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        stack.axis = .vertical
+        stack.spacing = 2
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - API
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    func configure() {
+        guard let user = user else { return }
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl)
+        
+        usernameLabel.text = user.username
+        fullnameLabel.text = user.fullname
+    }
+}
